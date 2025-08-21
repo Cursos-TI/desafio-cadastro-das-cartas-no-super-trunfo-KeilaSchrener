9aaf9f3cacbfd84b87daa67cef420472f3c41a8a
KeilaSchrener <keilaawmsul@gmail.com>
2025-08-21 20:03:33 +0000

    Finalização da criação das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,39 +4,71 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
     // Definição das variáveis carta 1
     char Estado1[20];
-    char Codigo_carta1[3];
+    char Codigo_carta1[4];
     char Nome_Cidade1[20];
     int Populacao1;
-    float Area_Km²1;
     float PIB1;
+    float Area_Km2_1;
     int Quantidade_Pontos_Turisticos1;
 
     printf("Primeira carta \n");
+
     printf("Nome do Estado: ");
-    scanf(" %s", Estado1);
+    scanf(" %19s", Estado1);
 
     printf("Código da Carta: ");
-    scanf(" %s", Codigo_carta1);
+    scanf(" %3s", Codigo_carta1);
 
     printf("Nome da Cidade: ");
-    scanf(" %s", Nome_Cidade1);
+    scanf(" %19s", Nome_Cidade1);
 
-    printf("População: ");
+    printf("População (Em milhares, utilize um número inteiro): ");
     scanf(" %d", &Populacao1);
 
-    printf("Área em Km²: ");
-    scanf(" %f", &Area_Km²1);
-
-    printf("PIB: ");
+    printf("PIB (Em bilhões, utilize ponto como separador): ");
     scanf(" %f", &PIB1);
+
+    printf("Área em Km²: ");
+    scanf(" %f", &Area_Km2_1);
 
     printf("Quantidade de pontos turísticos: ");
     scanf(" %d", &Quantidade_Pontos_Turisticos1);
+
+    // Definição das variáveis carta 2
+    char Estado2[20];
+    char Codigo_carta2[4];
+    char Nome_Cidade2[20];
+    int Populacao2;
+    float PIB2;
+    float Area_Km2_2;
+    int Quantidade_Pontos_Turisticos2;
+
+    printf("Segunda carta \n");
+
+    printf("Nome do Estado: ");
+    scanf(" %19s", Estado2);
+
+    printf("Código da Carta: ");
+    scanf(" %3s", Codigo_carta2);
+
+    printf("Nome da Cidade: ");
+    scanf(" %19s", Nome_Cidade2);
+
+    printf("População (Em milhares, utilize um número inteiro): ");
+    scanf(" %d", &Populacao2);
+
+    printf("PIB (Em bilhões, utilize ponto como separador): ");
+    scanf(" %f", &PIB2);
+
+    printf("Área em Km²: ");
+    scanf(" %f", &Area_Km2_2);
+
+    printf("Quantidade de pontos turísticos: ");
+    scanf(" %d", &Quantidade_Pontos_Turisticos2);
 
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
@@ -49,6 +81,7 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
+    
+    return 0;
     
 }
